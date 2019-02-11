5408360ef4b56bb3c784e1a0fe338d897924158e
guyburin <guyfreedom02@gmail.com>
2019-02-11 21:05:16 +0700

    update not have new

--- labproject/src/input.java
+++ labproject/src/input.java
@@ -1,4 +1,44 @@
 
 public class input {
+	private int salary;
+	private String name,id;
+	
+	public input() {
+		
+	}
 
+	public input(int salary, String name, String id) {
+		
+		this.salary = salary;
+		this.name = name;
+		this.id = id;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
+	
 }
--- labproject/src/test.java
+++ labproject/src/test.java
@@ -2,7 +2,8 @@
 public class test {
 
 	public static void main(String[] args) {
-		
+		input emp=new input();
+		System.out.println("");
 
 	}
 
